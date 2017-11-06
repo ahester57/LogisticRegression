252a745be9daf0913e09409a241d72f593c2cb78
Austin Hester <ahester57@gmail.com>
2017-11-05 23:45:00 -0600

    2nd input column added

--- 2logisticregr.py
+++ 2logisticregr.py
@@ -4,6 +4,7 @@
 # 11.05.17
 
 import numpy as np
+
 # define x_0
 x0 = 1
 
@@ -15,11 +16,12 @@ def ddw(i, x, y, w_, c):
     # for d/dw1 ln L
     for j in range(1,9):
         
-        eexp = np.exp( ( w_[0] * x0 ) + ( w_[1] * x[j-1] ) ) 
+        eexp = np.exp( ( w_[0] * x0 ) + ( w_[1] * x[j-1][0] ) +
+                            ( w_[2] * x[j-1][1]) ) 
         if (i == 0):
             pointmult = 1
         else:
-            pointmult = x[j-1]
+            pointmult = x[j-1][i-1]
         point = pointmult * (y[j-1] - ( eexp / ( 1 + eexp ) ) ) 
         s = s + point
     w = w_[i] + (c * s)
@@ -27,18 +29,20 @@ def ddw(i, x, y, w_, c):
 
 # compute the passing chance given x weeks of inactivity
 def passingchance(w_, x):
-    chance = 1 / ( 1 + np.exp(x0 * w_[0] + (x * w_[1])))
+    chance = 1 / ( 1 + np.exp( x0 * w_[0] + (x[0] * w_[1]) + (x[1]*w_[2])))
     return chance
 
-# input data [1-8], "weeks of inactivity"
+# input data [1-8], "weeks of inactivity" , " avg AP score; 0=N/A"
 #x = np.arange(1, 9)
-x = np.array( [1,2,3,3,4,5,6,7,8] )
+x = np.array( [[1,1,2,3,3,4,4,5,5,6,7,8],
+               [0,4,2,0,0,2,4,5,3,3,4,5]])
+x = x.T
 # output, 0 = "pass", 1 = "fail"
-y = np.array( [0,1,0,1,1,0,1,1,1] )
+y = np.array(  [0,0,1,0,1,1,0,0,1,1,1,1] )
 # step size
-c = 0.01
+c = 0.1
 # initial weight vector
-w_ = np.array( [1., 1.] )
+w_ = np.array( [1., 1., 1.] )
 
 # iterate T times
 T = 2000
@@ -49,13 +53,18 @@ for t in range(T):
     print("---------------------------")
     new_w1 = ddw(1,x,y,w_,c)
     print(new_w1)
+    print("---------------------------")
+    new_w2 = ddw(2,x,y,w_,c)
+    print(new_w2)
     w_[0] = new_w0
     w_[1] = new_w1
+    w_[2] = new_w2
     print("===========================")
 
 # print weight vector
 print("\nWeight vector: ", w_)
-print("\nWeeks of Inactivity\tChances of passing")
-for i in range(0,13):
-    print("\t",i, "\t\t", round(passingchance(w_, i),4)*100, "%")
+print("\nWeeks of Inactivity")
+print("x avg AP exam\t\tChances of passing")
+for i in range(0,12):
+    print("\t",x[i], "\t\t", round(passingchance(w_, x[i]),4)*100, "%")
 
